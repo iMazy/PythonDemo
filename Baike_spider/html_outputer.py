3d2e76c23b35b798b364c3b1d6f735a63609ad4e
Mazy <mazongyong@163.com>
2017-12-18 14:25:00 +0800

    feat: finish html output file

--- Baike_spider/html_outputer.py
+++ Baike_spider/html_outputer.py
@@ -3,3 +3,25 @@
 # @Author  : Mazy
 # @File    : html_outputer.py
 # @Software: PyCharm
+
+class HtmlOutputer(object):
+
+    def __init__(self):
+        self.datas = []
+
+    def collect_data(self, data):
+        if  data is None:
+            return
+        self.datas.append(data)
+
+    def output_html(self):
+        file = open('output.txt','w+', encoding='utf-8')
+
+        for  data in self.datas:
+            file.write("-----------------------------------------------------------------\n")
+            file.write(data['title'] + '\n')
+            file.write(data['url'] + '\n')
+            file.write(data['summary'] + '\n')
+
+
+        print("输出完成，请打开当前文件目录下的 output.txt 文件查看内容")
