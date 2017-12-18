1b6e4c5f875dc543ece81ab2cf892b6831c3dd0a
Mazy <mazongyong@163.com>
2017-12-18 14:08:36 +0800

    feat: finish htmml downloader file

--- Baike_spider/html_downloader.py
+++ Baike_spider/html_downloader.py
@@ -3,3 +3,15 @@
 # @Author  : Mazy
 # @File    : html_downloader.py
 # @Software: PyCharm
+
+import urllib.request
+
+class HtmlDownloader(object):
+
+    def html_downloader(self, url):
+        if url is None:
+            return
+
+        with urllib.request.urlopen(url) as response:
+            html = response.read().decode('utf-8')
+            return html
